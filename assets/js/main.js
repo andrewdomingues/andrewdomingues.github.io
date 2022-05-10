3ff7c9f3f2700ac7416d676f550840217603f1fc
Andy Domingues <andrewluisdomingues@protonmail.com>
2022-05-10 09:24:05 -0300

    Finished Mobile MenuBar with accessibility

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+const btnMb = document.querySelector('#btn-mb')
+
+function toggleMenu(event){
+    if(event.type === 'touchstart') event.preventDefault()
+    const menu = document.querySelector('nav')
+    menu.classList.toggle('show-menu')
+
+    const active = menu.classList.contains('show-menu')
+    console.log(active)
+
+    event.currentTarget.setAttribute('aria-expanded', active)
+
+    if(active) {
+        event.currentTarget.setAttribute('aria-label', 'Close menu')
+    } else {
+        event.currenTarget.setAttribute('aria-label', 'Open menu')
+    }
+}
+
+btnMb.addEventListener('click', toggleMenu)
+btnMb.addEventListener('touchstart', toggleMenu)
