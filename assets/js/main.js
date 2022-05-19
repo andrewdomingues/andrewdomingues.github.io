53a4f92d1e0206e96f84addd31136e593fb8cf59
Andrew Domingues <andrewluisdomingues@protonmail.com>
2022-05-19 16:43:14 -0300

    all work

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,15 @@
 const btnMb = document.querySelector('#btn-mb')
-
 function toggleMenu(event){
     if(event.type === 'touchstart') event.preventDefault()
     const menu = document.querySelector('nav')
     menu.classList.toggle('show-menu')
-
     const active = menu.classList.contains('show-menu')
-    console.log(active)
-
     event.currentTarget.setAttribute('aria-expanded', active)
-
     if(active) {
-        event.currentTarget.setAttribute('aria-label', 'Close menu')
+        event.currentTarget.setAttribute('aria-label', 'Fechar menu')
     } else {
-        event.currentTarget.setAttribute('aria-label', 'Open menu')
+        event.currentTarget.setAttribute('aria-label', 'Abrir menu')
     }
 }
-
 btnMb.addEventListener('click', toggleMenu)
 btnMb.addEventListener('touchstart', toggleMenu)
