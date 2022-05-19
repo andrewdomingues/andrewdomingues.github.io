c020e4ddadad03f7364f0e8391bffd090b913609
Andrew Domingues <andrewluisdomingues@protonmail.com>
2022-05-19 16:05:52 -0300

    not work

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,13 @@ const btnMb = document.querySelector('#btn-mb')
 function toggleMenu(event){
     if(event.type === 'touchstart') event.preventDefault()
     const menu = document.querySelector('nav')
-    menu.classList.toggle('show-menu')
-
     const active = menu.classList.contains('show-menu')
-    console.log(active)
-
+    menu.classList.toggle('show-menu')
     event.currentTarget.setAttribute('aria-expanded', active)
-
     if(active) {
         event.currentTarget.setAttribute('aria-label', 'Close menu')
     } else {
-        event.currenTarget.setAttribute('aria-label', 'Open menu')
+        event.currentTarget.setAttribute('aria-label', 'Open menu')
     }
 }
 btnMb.addEventListener('click', toggleMenu)
