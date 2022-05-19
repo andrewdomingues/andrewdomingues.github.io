4590ba00c12566ca705f4afad759652db968f705
Andrew Domingues <andrewluisdomingues@protonmail.com>
2022-05-19 16:28:01 -0300

    Revert to commit 70bbc63

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,21 @@
 const btnMb = document.querySelector('#btn-mb')
+
 function toggleMenu(event){
     if(event.type === 'touchstart') event.preventDefault()
     const menu = document.querySelector('nav')
-    const active = menu.classList.contains('show-menu')
     menu.classList.toggle('show-menu')
+
+    const active = menu.classList.contains('show-menu')
+    console.log(active)
+
     event.currentTarget.setAttribute('aria-expanded', active)
+
     if(active) {
         event.currentTarget.setAttribute('aria-label', 'Close menu')
     } else {
         event.currentTarget.setAttribute('aria-label', 'Open menu')
     }
 }
+
 btnMb.addEventListener('click', toggleMenu)
 btnMb.addEventListener('touchstart', toggleMenu)
